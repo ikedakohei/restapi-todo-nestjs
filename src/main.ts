85e202eb16ae137694a539f2b4a66e57f8a88a26
kohei <kouhei.ikeda@veriserve.co.jp>
2023-04-28 14:44:45 +0900

    Prisma Serviceの実装

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { Request } from 'express';
+import * as cookieParser from 'cookie-parser';
+import * as csurf from 'csurf';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors({
+    credentials: true,
+    origin: ['http://localhost:3000'],
+  });
+  app.use(cookieParser());
   await app.listen(3005);
 }
 bootstrap();
