4d4cccacccdef559397c48bcf3c28dddd843e12f
kohei <kouhei.ikeda@veriserve.co.jp>
2023-05-09 10:12:02 +0900

    改行追加

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
+
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor(private readonly config: ConfigService) {
